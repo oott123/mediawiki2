b836f62a53e4b6e134a933e80c966ee7acf57a27
oott123 <git@public.oott123.com>
2019-09-01 14:17:34 +0800

    chroe: cleanup

--- examples/login.ts
+++ examples/login.ts
@@ -1,18 +1,20 @@
 import { MWBot } from '../src'
 
 const bot = new MWBot('https://ff14.huijiwiki.com/w/api.php')
-bot.login(process.env.MW_USERNAME, process.env.MW_PASSWORD)
-// .then(() => {
-//   return bot.edit({
-//     title: '用户:云泽宛风/沙盒/机器人编辑测试',
-//     text: 'The quick brown fox jumps over the lazy dog.',
-//     bot: true
-//   })
-// })
-// .then(() => {
-//   return bot.simpleUpload({
-//     filename: 'sandbox_yunzewanfeng.png',
-//     file: '/Users/oott123/Documents/Work/我/m-sk7cvaIWeF-DwE6UH7Y9jCJQ.png',
-//     comment: '只是一次测试'
-//   })
-// })
+// tslint:disable-next-line: no-floating-promises
+bot
+  .login(process.env.MW_USERNAME, process.env.MW_PASSWORD)
+  .then(() => {
+    return bot.edit({
+      title: 'Project:Sandbox/test',
+      text: 'The quick brown fox jumps over the lazy dog.',
+      bot: true
+    })
+  })
+  .then(() => {
+    return bot.simpleUpload({
+      filename: 'sandbox_yunzewanfeng.png',
+      file: 'example.png',
+      comment: '只是一次测试'
+    })
+  })
